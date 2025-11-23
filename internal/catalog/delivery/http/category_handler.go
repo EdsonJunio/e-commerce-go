f6f1b5ca5a020b8d0045b0ec84ecc82ed141d24d
Edson Almeida <edson.ribeiro@onfly.com.br>
2025-11-22 23:51:56 -0300

    feat: test

--- internal/catalog/delivery/http/category_handler.go
+++ internal/catalog/delivery/http/category_handler.go
@@ -240,7 +240,7 @@ func (h *CategoryHandler) UpdateCategory(c *gin.Context) {
 	var req updateCategoryRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
 		logger.L().Warn(
-			"invalid request body in UpdateCategory",
+			"invalid request body in updateCategory",
 			zap.Error(err),
 			zap.String("request_id", reqID),
 		)
